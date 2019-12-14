f1b82c7ac2c213dfa040eca5c6596d160b05c711
Nick Visalli <nvisal1@students.towson.edu>
2019-12-14 15:16:36 -0500

    refactor multiple linear regression script, accept data set path as arg, save model to ./REST-API

--- Multiple_Linear_Regression/multiple_linear_regression.py
+++ Multiple_Linear_Regression/multiple_linear_regression.py
@@ -1,72 +1,76 @@
 # Multiple Linear Regression
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 4].values
-
-print(X)
-
-# Encoding categorical data
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import make_column_transformer
-
-onehotencoder = make_column_transformer((StandardScaler(), [0, 1, 2]), (OneHotEncoder(), [3]))
-X = onehotencoder.fit_transform(X)
-
-print(X)
-
-# Avoiding the Dummy Variable Trap
-X = X[:, :-1]
-
-# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-# Fitting Multiple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+import statsmodels.api as sm
+import sys
+import joblib
+
+def main(dataset_path):
+    dataset = pd.read_csv(dataset_path)
+
+    X, Y = getXAndYValues(dataset)
+
+    X = encodeCategoricalData(X)
+
+    # Avoid the Dummy Variable Trap
+    X = X[:, :-1]
+
+    significant_variables = determineSignificantVariables(X, Y)
+
+    # fit the model using significant variables
+    model = sm.OLS(Y, significant_variables).fit()
+
+    saveModelToFile(model, path = '../REST-API/model.sav')
+
+    # dataset = pd.read_csv(dataset_path)
+    # new = dataset.iloc[:, :-1].values
+    # new[1,0] = 165349
+    # onehotencoder = make_column_transformer((StandardScaler(), [0, 1, 2]), (OneHotEncoder(), [3]))
+    # new = onehotencoder.fit_transform(new)
+
+    # # model input is R & D Spend
+    # # Predicting the Test set results
+    # y_pred = model.predict([[1, new[1,0]]])
+
+    # print(y_pred)
+
+def getXAndYValues(dataset):
+    X = dataset.iloc[:, :-1].values
+    Y = dataset.iloc[:, 4].values
+    return X, Y
+
+def encodeCategoricalData(X):
+    onehotencoder = make_column_transformer((StandardScaler(), [0, 1, 2]), (OneHotEncoder(), [3]))
+    X = onehotencoder.fit_transform(X)
+    return X
+
+def determineSignificantVariables(X, Y):
+    significance_level = 0.05
+    X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+    X = X[:, [0, 1, 2, 3, 4, 5]]
+    significant_variables = performBackwardsElimination(X, Y, significance_level)
+    return significant_variables
 
 # Building the optimal model using Backward Elimination
-import statsmodels.api as sm
-def backwardsElimination(data, significance_level):
-    numberOfVariables = len(data[0])
+def performBackwardsElimination(X, Y, significance_level):
+    numberOfVariables = len(X[0])
     for i in range(0, numberOfVariables):
-        regressor_OLS = sm.OLS(y, data).fit()
+        regressor_OLS = sm.OLS(Y, X).fit()
         highestPValue = max(regressor_OLS.pvalues).astype(float)
         if highestPValue > significance_level:
             for j in range(0, numberOfVariables - i):
                 if (regressor_OLS.pvalues[j].astype(float) == highestPValue):
-                    data = np.delete(data, j, 1)
+                    X = np.delete(X, j, 1)
     print(regressor_OLS.summary())
-    return data
+    return X
 
-significance_level = 0.05
-X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
-data = X[:, [0, 1, 2, 3, 4, 5]]
-model_data = backwardsElimination(data, significance_level)
+def saveModelToFile(model, path):
+    joblib.dump(model, path)
 
-
-
-# fit the model again 
-model = sm.OLS(y, model_data).fit()
-
-X = dataset.iloc[:, :-1].values
-X[1,0] = 165349
-onehotencoder = make_column_transformer((StandardScaler(), [0, 1, 2]), (OneHotEncoder(), [3]))
-X = onehotencoder.fit_transform(X)
-
-print(X[1,0])
-
-# model input is R & D Spend
-
-# Predicting the Test set results
-y_pred = model.predict([[1, X[1,0]]])
-
-print(y_pred)
+if __name__ == "__main__":
+   main(sys.argv[1])
